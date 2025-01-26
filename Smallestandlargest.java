d22ab55fbec76fea2440f9daba2a174ceb94e8be
Shaiksaniya06 <sksaniya2006@gmail.com>
2025-01-26 13:46:02 +0530

    Add files via upload

---
+++ Smallestandlargest.java
@@ -0,0 +1,17 @@
+public class Smallestandlargest {
+    public static void main(String[]args){
+        int[] number={12,45,65,9,11};
+        int min=number[0];
+        int max=number[0];
+        for(int i=1;i<number.length;i++){
+            if(number[i]<min){
+                min=number[i];
+            }
+            if(number[i]>max){
+                max=number[i];
+            }
+        }
+    System.out.println("smallest value:" + min);
+    System.out.println("largest value:" + max);
+    }
+}
